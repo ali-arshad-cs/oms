6614efd7105fd662d95cd4d381d7840de010c103
Ali Arshad <ali@Ali-Arshad.local>
2023-11-26 07:35:09 +0500

    added methods of signup and signin

--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,10 @@
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from . import views
 from django.contrib.auth.models import User
 from django.contrib import messages
+
 
 # Create your views here.
 def home(request):
@@ -10,7 +12,6 @@ def home(request):
 
 
 def signup(request):
-
     if request.method == "POST":
         username = request.POST['username']
         fname = request.POST['fname']
@@ -26,11 +27,24 @@ def signup(request):
         messages.success(request, 'Your Account Successfully Created')
         return redirect('signin')
 
-
     return render(request, "authentication/signup.html")
 
+
 def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        pass1 = request.POST['pass1']
+
+        user = authenticate(username=username, password=pass1)
+        if user is not None:
+            login(request, user)
+            fname = User.first_name
+            return render(request, "authentication/index.html", {'fname': fname})
+        else:
+            messages.error(request, "Username and Password wrong")
+            return redirect('signup')
     return render(request, "authentication/signin.html")
+
 
 def signout(request):
     pass
