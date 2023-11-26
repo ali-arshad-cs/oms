e41e295bf434879acede02274e1a5edd8cf21b49
Ali Arshad <ali@Ali-Arshad.local>
2023-11-26 07:08:54 +0500

    created sign up page

--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from . import views
-
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -9,6 +10,23 @@ def home(request):
 
 
 def signup(request):
+
+    if request.method == "POST":
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+        myuser.save()
+        messages.success(request, 'Your Account Successfully Created')
+        return redirect('signin')
+
+
     return render(request, "authentication/signup.html")
 
 def signin(request):
